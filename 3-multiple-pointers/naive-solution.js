8ed171b6dd32fdd343c813c13fb2b68e02dfc98c
Joliver Estampador <joliverestampador@gmail.com>
2020-03-31 19:08:23 +0800

    refactored

--- 3-multiple-pointers/naive-solution.js
+++ 3-multiple-pointers/naive-solution.js
@@ -1,10 +1,26 @@
-// naive solution
+// // naive solution
+// function sumZero(arr) {
+//     for (let i = 0; i < arr.length; i++) {
+//         for (let j = 0; j < arr.length; j++) {
+//             if (arr[i] + arr[j] === 0) {
+//                 return [arr[i], arr[j]];
+//             }
+//         }
+//     }
+// }
+
+// refactored
 function sumZero(arr) {
-    for (let i = 0; i < arr.length; i++) {
-        for (let j = 0; j < arr.length; j++) {
-            if (arr[i] + arr[j] === 0) {
-                return [arr[i], arr[j]];
-            }
+    let left = 0;
+    let right = arr.length - 1;
+    while (left < right) {
+        let sum = arr[left] + arr[right];
+        if (sum === 0) {
+            return [arr[left], arr[right]];
+        } else if (sum > 0) {
+            right--;
+        } else {
+            left++;
         }
     }
 }
