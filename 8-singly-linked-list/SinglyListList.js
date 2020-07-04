9cc6ce24c2d3a138b47486175a96bd640353b5b2
Joliver Estampador <joliverestampador@gmail.com>
2020-07-04 13:32:23 +0800

    get()

--- 8-singly-linked-list/SinglyListList.js
+++ 8-singly-linked-list/SinglyListList.js
@@ -101,4 +101,22 @@ class SinglyLinkedList {
         this.length++;
         return this;
     }
+
+    /*
+	* this function should accept an index
+	* if the index is less than zero or greater than or equal to the length of the list, return null
+	* Loop thru the list until you reach the index and return the node at that specific index
+    */
+   get(index) {
+        if (index < 0 || index >= this.length) return null;
+        let counter = 0;
+
+        let current = this.head;
+        while (counter !== index) {
+            current = current.next;
+            counter++;
+        }
+
+        return current;
+    }
 }
