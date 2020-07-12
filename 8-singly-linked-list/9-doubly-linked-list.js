dec294db572c9751d9a7cc23035b8f2e40357868
Joliver Estampador <joliverestampador@gmail.com>
2020-07-12 13:26:02 +0800

    push()
        /*
    	* Create a new node with the value passed to the the function
    	* If the head property is null, set the head and tail to be the newy created node
    	* if not, set the tail next property to be the node
    	* Set the newly created node prev property to be the to-be-old tail
    	* Set the tail to be the newly created node
    	* return list
        */

--- 8-singly-linked-list/9-doubly-linked-list.js
+++ 8-singly-linked-list/9-doubly-linked-list.js
@@ -12,4 +12,26 @@ class DoublyLinkedList {
         this.tail = null;
         this.length = 0;
     }
+
+    /* 
+	* Create a new node with the value passed to the the function
+	* If the head property is null, set the head and tail to be the newy created node
+	* if not, set the tail next property to be the node
+	* Set the newly created node prev property to be the to-be-old tail
+	* Set the tail to be the newly created node
+	* return list
+    */
+    push(val) {
+        const node = new Node(val);
+        if (!this.head) {
+            this.head = node;
+            this.tail = node;
+        } else {
+            this.tail.next = node;
+            node.prev = this.tail;
+            this.tail = node;
+        }
+        this.length++;
+        return this;
+    }
 }
