3b59b5777d8e8bbb46c1e7439d281c8d019fd311
Joliver Estampador <joliverestampador@gmail.com>
2020-07-18 20:20:47 +0800

    enqueue() and dequeue()

---
+++ 10-stacks-and-queues/Queue.js
@@ -0,0 +1,39 @@
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+class Queue {
+    constructor() {
+        this.first = null;
+        this.last = null;
+        this.size = 0;
+    }
+
+    enqueue(val) {
+        const newNode = new Node(val)
+        if (!this.first) {
+            this.first = newNode;
+            this.last = newNode;
+        } else {
+            this.last.next = newNode;
+            this.last = newNode;
+        }
+
+        return ++this.size;
+    }
+
+    // identical with Stack pop()
+    dequeue() {
+        if (!this.first) return null;
+        const temp = this.first;
+        if (this.first === this.last) {
+            this.last = null;
+        }
+        this.first = this.first.next;
+        this.size--;
+        return temp.val;
+    }
+}
