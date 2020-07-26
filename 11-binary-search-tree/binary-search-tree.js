d641cbadb8fed7fcbcc7f31e4cb1a3460855bba2
Joliver Estampador <joliverestampador@gmail.com>
2020-07-26 10:00:16 +0800

    bst insert()

--- 11-binary-search-tree/binary-search-tree.js
+++ 11-binary-search-tree/binary-search-tree.js
@@ -10,4 +10,34 @@ class BinarySearchTree {
     constructor() {
         this.root = null;
     }
+
+    insert(value) {
+        const newNode = new Node(value);
+        if (!this.root) {
+            this.root = newNode;
+            return this;
+        } 
+
+        let currentRoot = this.root;
+
+        while(true) {
+            // handle duplicates
+            if (value === currentRoot.value) return undefined;
+            if (value > currentRoot.value) {
+                if (!currentRoot.right) {
+                    currentRoot.right = newNode;
+                    return this;
+                }
+
+                currentRoot = currentRoot.right;
+            } else {
+                if (!currentRoot.left) {
+                    currentRoot.left = newNode;
+                    return this;
+                }
+
+                currentRoot = currentRoot.left;
+            }
+        }
+    }
 }
