232f3c5d0333cc1bc7fc58c3dab7a8cd7c1d1d88
Joliver Estampador <joliverestampador@gmail.com>
2020-08-08 10:01:15 +0800

    breadthFirstSearch()

--- 11-binary-search-tree/binary-search-tree.js
+++ 11-binary-search-tree/binary-search-tree.js
@@ -58,4 +58,28 @@ class BinarySearchTree {
         }
         return false;
     }
+
+     /**
+	* Create a queue (JS Array) and a variable to store the values of nodes visited
+	* Place the root node in the queue
+	* Loop as long as there is anything in the queue
+		* Dequeue a node from the queue and push the value of the node into the variable that stores the nodes
+		* If there is a left property on the node dequeued - add it to the queue
+		* If there is a right property on the node dequeued - add it to the queue
+	* Return the variable that stores the values
+    */
+   breadthFirstSearch() {
+        if (!this.root) return [];
+        let queue = [this.root];
+        let visited = [];
+
+        while(queue.length) {
+            const dequeuedNode = queue.shift()
+            visited.push(dequeuedNode.value)
+            if (dequeuedNode.left) queue.push(dequeuedNode.left)
+            if (dequeuedNode.right) queue.push(dequeuedNode.right)
+        }
+        
+        return visited;
+    }
 }
