567965bb8f3c097471a766d0554cdbbe4d732236
Joliver Estampador <joliverestampador@gmail.com>
2020-09-27 11:36:20 +0800

    extractMax() or removing max value

--- 12-heaps/MaxBinaryHeap.js
+++ 12-heaps/MaxBinaryHeap.js
@@ -44,7 +44,78 @@ class MaxBinaryHeap {
     findParent(index) {
         return Math.floor((index - 1) / 2);
     }
+
+    /*  
+	* Swap the first value in the values property with the last one
+	* Pop from the values property, so you can return the value at the end.
+	* Have the new root "sink down" to the correct spot
+    */
+    extractMax() {
+    	const maxValue = this.values[0];
+    	const lastValue = this.values.pop();
+    	// fix edge case for 1 item list
+    	if (this.values.length > 0) {
+			this.values[0] = lastValue;
+			this.sinkDown();
+    	}
+        return maxValue;
+    }
+
+    /*
+    * sinking down
+		* Your parent index starts at 0 (the root)
+		* Find the index of the left child: 2 * index + 1 (makes sure it's not out of bounds)
+		* Find the index of the right child: 2 * index + 1 (makes sure it's not out of bounds)
+		* If the left or right child is greater than the element... swap. 
+		    If both left and right children are larger, swap with the largest child
+		* The child index you swapped to now becomes the new parent index.
+		* Keep looping and swapping until neither child is larger than the element
+		* return the old root
+    */
+    sinkDown() {
+    	const { values } = this;
+    	let parentIdx = 0;
+    	const element = values[parentIdx];
+    	const length = values.length;
+
+    	while(true) {
+    		const leftChildIdx = (2 * parentIdx) + 1;
+    		const rightChildIdx = (2 * parentIdx) + 2;
+
+    		let leftChild, rightChild;
+    		let swap = null;
+
+    		if (leftChildIdx < length) {
+    			leftChild = values[leftChildIdx];
+                if (leftChild > element) {
+                	swap = leftChildIdx;
+                }
+    		}
+
+    		if (rightChildIdx < length) {
+    			rightChild = values[rightChildIdx];
+    			if (
+    			    (swap === null && rightChild > element) ||
+    			    (swap !== null && rightChild > leftChild)
+    			) {
+                    swap = rightChildIdx;
+    			}
+    		}
+
+    		if (swap === null) break;
+    		// swapping
+    		values[parentIdx] = values[swap];
+    		values[swap] = element;
+    		parentIdx = swap;
+    	}
+    }
 }
 
 const maxBinaryHeap = new MaxBinaryHeap();
-maxBinaryHeap.values = [41, 39, 33, 18, 27, 12];
+maxBinaryHeap.insert(41);
+maxBinaryHeap.insert(39);
+maxBinaryHeap.insert(33);
+maxBinaryHeap.insert(18);
+maxBinaryHeap.insert(27);
+maxBinaryHeap.insert(12);
+maxBinaryHeap.insert(55);
