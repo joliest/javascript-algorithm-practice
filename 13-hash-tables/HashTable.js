afafa50b29592dbf2ec69b2f6ea691e3f07efd0a
Joliver Estampador <joliverestampador@gmail.com>
2020-10-18 14:00:57 +0800

    hash set()

--- 13-hash-tables/HashTable.js
+++ 13-hash-tables/HashTable.js
@@ -9,8 +9,23 @@ class HashTable {
         for (let i = 0; i < Math.min(key.length, 100); i++) {
             let char = key[i];
             let value = char.charCodeAt(0) - 96;
-            total = (total * WEIRD_PRIME + value) % this.keyMap.length
+            total = (total * WEIRD_PRIME + value) % this.keyMap.length;
         }
         return total;
     }
+
+    set(key, value) {
+        const index = this._hash(key);
+        // separate chaining
+        if (!this.keyMap[index]) {
+            this.keyMap[index] = [];
+        }
+        this.keyMap[index].push([key, value]);
+    }
 }
+
+const hash = new HashTable();
+hash.set('hello world!', 'goodbye!!');
+hash.set('dogs', 'are cool');
+hash.set('cats', 'are fine');
+hash.set('i love', 'pizza');
