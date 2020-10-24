b093310629efc8649eb187b580a4937527fd1703
Joliver Estampador <joliverestampador@gmail.com>
2020-10-24 13:50:46 +0800

    addEdge()
        /**
    	* this function should accept two vertices, we can call them vertex1 and vertex2
    	* This function should find in the adjacency list the key of vertex1 and push vertex2 to to the array
    	* The function should find in the adjacency list the key of vertext2 and push vertex1 to the array
    	* Dont worry about handling errors or invalid vertices
        */

--- 14-graphs/Graph.js
+++ 14-graphs/Graph.js
@@ -13,7 +13,22 @@ class Graph {
             this.adjacencyList[name] = [];
         }
     }
+
+    /**
+	* this function should accept two vertices, we can call them vertex1 and vertex2
+	* This function should find in the adjacency list the key of vertex1 and push vertex2 to to the array
+	* The function should find in the adjacency list the key of vertext2 and push vertex1 to the array
+	* Dont worry about handling errors or invalid vertices
+    */
+    addEdge(vertex1, vertex2) {
+        // add validation
+        // if this is directed, you dont need the second line
+        this.adjacencyList[vertex1].push(vertex2);
+        this.adjacencyList[vertex2].push(vertex1);
+    }
 }
 
 const graph = new Graph();
 graph.addVertex('joli');
+graph.addVertex('arlene');
+graph.addVertex('khaye');
