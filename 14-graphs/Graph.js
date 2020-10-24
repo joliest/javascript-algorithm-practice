7e612d768cd4b61de26025160dd1aff91e48e406
Joliver Estampador <joliverestampador@gmail.com>
2020-10-24 13:38:57 +0800

    addVertex()
    	*
    write a method called addVertex() which accepts a name of a vertex
    	*
    It should add a key to an adjacency list with the name of the vertex and set its value to be an empty array.

--- 14-graphs/Graph.js
+++ 14-graphs/Graph.js
@@ -2,4 +2,18 @@ class Graph {
     constructor() {
         this.adjacencyList = {};
     }
+
+    /**
+	* write a method called addVertex() which accepts a name of a vertex
+	* It should add a key to an adjacency list with the name of the vertex and 
+	set its value to be an empty array.
+    */
+    addVertex(name) {
+        if (!this.adjacencyList[name]) {
+            this.adjacencyList[name] = [];
+        }
+    }
 }
+
+const graph = new Graph();
+graph.addVertex('joli');
