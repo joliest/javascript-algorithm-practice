5707e62c06068068e0c0342ecd2d2692c01f123c
Joliver Estampador <joliverestampador@gmail.com>
2020-11-01 15:51:22 +0800

    breadth first
    	This function should accept a starting vertex
    	Create a queue (you can use an array) and place the starting vertex in it
    	Create a result array we return later
    	Create an object to store nodes visited
    	Mark the starting vertex as visited
    	Loop as long as there is anything i the queue
    	Remove the first vertex (shift) from the queue and push it into the result list
    	Loop over each vertex in the adjacency list for the vertex you are visiting
    	If it is not inside the object that stores nodes visited, mark it as visited and enqueue that vertex

--- 14-graphs/Graph.js
+++ 14-graphs/Graph.js
@@ -123,6 +123,36 @@ class Graph {
     	}
     	return result;
     }
+
+    /**
+	This function should accept a starting vertex
+	Create a queue (you can use an array) and place the starting vertex in it
+	Create a result array we return later
+	Create an object to store nodes visited
+	Mark the starting vertex as visited
+	Loop as long as there is anything i the queue
+	Remove the first vertex (shift) from the queue and push it into the result list
+	Loop over each vertex in the adjacency list for the vertex you are visiting
+	If it is not inside the object that stores nodes visited, mark it as visited and enqueue that vertex
+    */
+    breadthFirstIterative(start) {
+    	const queue = [start];
+    	const result = [];
+    	const visited = {};
+    	visited[start] = true;
+    	let currentVertex;
+    	while(queue.length) {
+    		currentVertex = queue.shift();
+    		result.push(currentVertex);
+    		this.adjacencyList[currentVertex].forEach(neighbor => {
+    			if (!visited[neighbor]) {
+    				visited[neighbor] = true;
+    				queue.push(neighbor);
+    			}
+    		})
+    	}
+    	return result;
+    }
 }
 
 const g = new Graph();
