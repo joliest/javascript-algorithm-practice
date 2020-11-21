afd9e0e375ee4f8ec025bbc4b779e5f8e66e034c
Joliver Estampador <joliverestampador@gmail.com>
2020-11-21 11:16:21 +0800

    Simple Priority Queue

--- 15-Dijkstra/Dijkstra.js
+++ 15-Dijkstra/Dijkstra.js
@@ -3,14 +3,31 @@ class WeightedGraph {
         this.adjacencyList = {};
     }
 
-
     addVertex(vertex) {
         if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
     }
 
-
     addEdge(vertex1, vertex2, weight) {
         this.adjacencyList[vertex1].push({ node: vertex2, weight });
         this.adjacencyList[vertex2].push({ node: vertex1, weight });
+    }
+}
+
+class PriorityQueue {
+    constructor() {
+        this.values = [];
+    }
+
+    enqueue(val, priority) {
+        this.values.push({val, priority});
+        this.sort();
+    }
+
+    dequeue() {
+        return this.values.shift();
+    }
+
+    sort() {
+        this.values.sort((a, b) => a.priority - b.priority)
     }
 }
